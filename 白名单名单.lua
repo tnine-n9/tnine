d8a830ef1cc8d3bc43d6d30a4969e7f95b24c069
tnine-n9 <3562501187@qq.com>
2026-02-08 15:23:28 +0800

    Refactor whitelist format to JSON structure

--- 白名单名单.lua
+++ 白名单名单.lua
@@ -1,5 +1,12 @@
-[
-    "lookmexc",
-    "Bac0nByt32017",
-    "ylw0000"
-]
+  "active": true,
+  "message": "1213",
+  "whitelist_users": [
+    {
+      "username": "Bac0nByt32017",
+      "add_time": "2025/11/5 20:53:27"
+    }
+  ],
+  "blocked_users": [],
+  "version": "2.0.0",
+  "last_updated": "2025-11-07"
+}
