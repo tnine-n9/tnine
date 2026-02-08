a5b6ea26046ea46505a959c35f1301c754de7b1c
tnine-n9 <3562501187@qq.com>
2026-02-08 15:57:38 +0800

    Add multiple usernames to whitelist

--- 白名单名单.lua
+++ 白名单名单.lua
@@ -7,7 +7,71 @@
       "add_time": "2025/11/5 20:53:27"
     },
     {
-      "username": "12434",
+      "username": "9178Hjx7891",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "rwwjvgkvfkdyj",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "ylw0000",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "jmk00091",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "shsbsjs81",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "qazxplmn12345653",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "miiss386",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "lookmexc",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "",
       "add_time": "2025/12/8 16:47:28"
     }
   ],
